eba16616ac27c5ed955dc233d8968dbcf4c776e2
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-08 14:23:39 +0200

    authorization sin niveles de acceso

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,35 +1,36 @@
 package com.example.demo.security;
 
+import com.example.demo.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
+                .csrf(csrf -> csrf.disable()) // For development. In production, configure appropriately
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                        .requestMatchers("/api/admin").hasRole("ADMIN")
-                        .requestMatchers("/api/employee").hasRole("EMPLOYEE")
-                        .requestMatchers("/api/reservations/{guestId}").hasRole("GUEST")
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/public/**").permitAll()
+                    .requestMatchers("/api/auth/**").permitAll()
 
-                        .anyRequest().authenticated()
+                    .requestMatchers("/api/admin").hasRole("ADMIN")
+                    .requestMatchers("/api/employee").hasRole("EMPLOYEE")
+                    .requestMatchers("/api/reservations/{guestId}").hasRole("GUEST")
+                    .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
-    }
-
-@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
 
