9cb6f90b4102c78d2a6c8ec2a8db8ab104fcc42e
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-10 19:13:35 +0200

    modelo y controlador reservation

---
+++ src/main/java/com/example/demo/reservation/EReservationState.java
@@ -0,0 +1,8 @@
+package com.example.demo.reservation;
+
+public enum EReservationState {
+    RESERVED,
+    CONFIRMED,
+    CANCELLED,
+    PAST
+}
---
+++ src/main/java/com/example/demo/reservation/Reservation.java
@@ -0,0 +1,48 @@
+package com.example.demo.reservation;
+
+import com.example.demo.room.models.Room;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "reservations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long reservationId;
+
+    @Column(nullable = false )
+    private LocalDate arrivalDate;
+
+    @Column(nullable = false )
+    private LocalDate departureDate;
+
+    @Column(nullable = false )
+    private int pax;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EReservationState ReservationState;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_room", referencedColumnName = "room_id")
+    private Room roomReserved;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_guest", referencedColumnName = "guest_id")
+    private Guest reservedByGuest;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_employee", referencedColumnName = "employee_id")
+    private Employee createdByEmployee;
+
+    @Column(nullable = false )
+    private LocalDate creationDate;
+}
